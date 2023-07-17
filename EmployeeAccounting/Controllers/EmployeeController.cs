2cdaef8d319cad4852adeecd91a9a8b95eec4f11
evg2576 <24evgeniy03@gmail.com>
2023-07-17 13:58:52 +0300

    add model and controller methods

---
+++ EmployeeAccounting/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAccounting.Controllers;
+
+[Route("api/[controller]")]
+public class EmployeeController : ControllerBase
+{
+    static readonly List<Employee> data;
+
+    static EmployeeController()
+    {
+        data = new List<Employee>
+            {
+                new Employee { Name = "Nekita", Salary = 1337, BonusAdded = true, IsPromoted = false },
+                new Employee { Name = "S3rg10", Salary = 5051, BonusAdded = true, IsPromoted = true },
+            };
+    }
+
+    [HttpGet]
+    public IEnumerable<Employee> Get() => data;
+
+    [HttpPost]
+    public IActionResult Post(Employee employee)
+    {
+        data.Add(employee);
+        return Ok(employee);
+    }
+
+    [HttpDelete("{name}")]
+    public IActionResult Delete(string name)
+    {
+        var employee = data.FirstOrDefault(x => x.Name == name);
+
+        if (employee == null)
+            return NotFound();
+
+        data.Remove(employee);
+
+        return Ok(employee);
+    }
+}
---
+++ EmployeeAccounting/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeAccounting;
+public class Employee
+{
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+    public bool IsPromoted { get; set; }
+    public bool BonusAdded { get; set; }
+}
