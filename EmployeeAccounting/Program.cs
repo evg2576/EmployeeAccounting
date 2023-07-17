963d3a6316675ea728c67d4b19a4db9b3e29a25a
evg2576 <24evgeniy03@gmail.com>
2023-07-17 14:33:58 +0300

    add swagger

--- EmployeeAccounting/Program.cs
+++ EmployeeAccounting/Program.cs
@@ -4,7 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
