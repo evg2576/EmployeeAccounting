2c5cad20448ccfe402d4af2171500fbbd954715b
Sergey <yuzvenko.sergey@gmail.com>
2023-07-22 16:33:48 +0300

    add pagination & refactoring code

---
+++ EmployeeAccounting/ClientApp/src/components/pagination/pagination.js
@@ -0,0 +1,38 @@
+﻿import { Component } from 'react';
+
+import './pagination.css';
+
+class Pagination extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            currentPage: 1
+        };
+    }
+
+    renderPaginationButtons = () => {
+        const buttons = [];
+        for (let page = 1; page <= this.props.countPages; page++) {
+            buttons.push(
+                <button
+                    key={page}
+                    onClick={() => this.props.onHandlePagination(page)}
+                    style={{ fontWeight: this.props.pageNumber === page ? 'bold' : 'normal' }}
+                >
+                    {page}
+                </button>
+            );
+        }
+        return buttons;
+    };
+   
+    render() {
+        return (
+            <div>
+                <div>{this.renderPaginationButtons()}</div>
+            </div>
+        );
+    }
+}
+
+export default Pagination;
--- EmployeeAccounting/ClientApp/src/index.js
+++ EmployeeAccounting/ClientApp/src/index.js
@@ -11,11 +11,9 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <React.StrictMode>
-        <BrowserRouter basename={baseUrl}>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>   
+    <BrowserRouter basename={baseUrl}>
+        <App />
+    </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
