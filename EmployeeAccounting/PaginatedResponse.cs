b2a6f76d8127f6523a76110237817d201528e7ef
evg2576 <24evgeniy03@gmail.com>
2023-07-23 22:17:57 +0300

    filter and search added

--- EmployeeAccounting/PaginatedResponse.cs
+++ EmployeeAccounting/PaginatedResponse.cs
@@ -2,6 +2,7 @@
 public class PaginatedResponse<T>
 {
     public int TotalItems { get; set; }
+    public int PromotedCount { get; set; }
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
     public IEnumerable<T> Items { get; set; }
